012c2ab603d96c14d77863a955a43d66bb3787dd
Micaël Dupuis <micaeldupuis@live.fr>
2022-05-23 14:20:49 +0200

    Ajout des tests pour la route GET /tache/:id

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -14,4 +14,23 @@ describe("NodeJS API", () => {
 			taches = JSON.parse(JSON.stringify(taches));
 			expect(data).toMatchObject(taches);
 	});
+
+	test("GET /taches/:id avec id existant", async () => {
+		const res = await request(app)
+			.get("/tache/628b77f07dd18e03f48f4fcf")
+			.expect(200)
+			.expect("Content-Type", /json/);
+		
+			const data = JSON.parse(res.text);
+			let tache = await Tache.findById("628b77f07dd18e03f48f4fcf");
+			tache = JSON.parse(JSON.stringify(tache));
+			expect(data).toMatchObject(tache);
+	});
+
+	test("GET /taches/:id avec id non existant", async () => {
+		const res = await request(app)
+			.get("/tache/test")
+			.expect(500)
+			.expect("Content-Type", /json/);
+	});
 });
